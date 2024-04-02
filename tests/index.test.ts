03429247dd3ef4c43d1eb51f8b9838e9331b18b5
afisman <afisman@gmail.com>
2024-04-02 21:40:52 +0200

    added more tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,8 +4,8 @@ import { app } from '../app';
 const authToken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkFsZWZ0YXUiLCJpYXQiOjE3MTIwNzEyNjksImV4cCI6MTcyMDcxMTI2OX0.vDPJQB-ich5_n9B_tewv4ViWlOg_FeI-duod39aVPws";
 const malformedJWT = 'Hello world'
 
-describe('Post Endpoints', () => {
-    it('shouldedit booking', async () => {
+describe('Put Endpoints for bookings', () => {
+    it('should try to edit booking with no token', async () => {
         const res = await request(app)
             .put('/bookings/edit/12')
             .send({
@@ -13,10 +13,7 @@ describe('Post Endpoints', () => {
             })
         expect(res.statusCode).toEqual(401)
     })
-})
-
-describe('Post Endpoints', () => {
-    it('shouldedit booking', async () => {
+    it('should try to edit booking with malformed token', async () => {
         const res = await request(app)
             .put('/bookings/edit/12')
             .set({ authorization: malformedJWT })
@@ -25,4 +22,43 @@ describe('Post Endpoints', () => {
             })
         expect(res.statusCode).toEqual(403)
     })
+    it('should try to edit booking with correct token', async () => {
+        const res = await request(app)
+            .put('/bookings/edit/12')
+            .set({ authorization: authToken })
+            .send({
+                name: 'test is cool',
+            })
+        expect(res.statusCode).toEqual(200)
+    })
 })
+
+describe('Put Endpoints for users', () => {
+    it('should try to edit booking with no token', async () => {
+        const res = await request(app)
+            .put('/users/edit/12')
+            .send({
+                name: 'test is cool',
+            })
+        expect(res.statusCode).toEqual(401)
+    })
+    it('should try to edit booking with malformed token', async () => {
+        const res = await request(app)
+            .put('/users/edit/12')
+            .set({ authorization: malformedJWT })
+            .send({
+                name: 'test is cool',
+            })
+        expect(res.statusCode).toEqual(403)
+    })
+    it('should try to edit booking with correct token', async () => {
+        const res = await request(app)
+            .put('/users/edit/12')
+            .set({ authorization: authToken })
+            .send({
+                name: 'test is cool',
+            })
+        expect(res.statusCode).toEqual(200)
+    })
+})
+
