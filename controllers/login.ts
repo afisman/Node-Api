38a43617d288e62bd3cebb7dff8d4900233d3757
afisman <afisman@gmail.com>
2024-04-03 13:10:08 +0200

    updated index.html

--- controllers/login.ts
+++ controllers/login.ts
@@ -4,7 +4,7 @@ import { generateAccessToken } from '../middleware/auth';
 
 export const loginController = express.Router();
 
-loginController.get("/", async (req: Request, res: Response) => {
+loginController.post("/", async (req: Request, res: Response) => {
     const { username, password } = req.body;
 
     const loginAccepted = await login({ username: username, password: password });
