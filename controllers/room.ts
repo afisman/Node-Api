4488cd16a6a82a1fc54df3b4de9171726d1865a3
afisman <afisman@gmail.com>
2024-04-12 14:38:14 +0200

    fixed next in controller

--- controllers/room.ts
+++ controllers/room.ts
@@ -3,55 +3,50 @@ import { createRoom, deleteRoom, editRoom, fetchAllRooms, fetchSingleRoom } from
 
 export const roomController = express.Router();
 
-roomController.get("/", async (_req: Request, res: Response) => {
+roomController.get("/", async (_req: Request, res: Response, next: NextFunction) => {
     try {
         const rooms = await fetchAllRooms()
         res.json(rooms);
     } catch (error) {
-        console.error('An error ocurred', error);
-        res.status(500).json({ error });
+        next(error);
     }
 })
 
-roomController.get("/:id", async (req: Request, res: Response, _next: NextFunction) => {
+roomController.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params;
     try {
         const singleRoom = await fetchSingleRoom(id)
         res.json(singleRoom);
     } catch (error) {
-        console.error('An error ocurred', error);
-        res.status(500).json({ error });
+        next(error);
     }
 })
 
-roomController.post("/", async (req: Request, res: Response, _next: NextFunction) => {
+roomController.post("/", async (req: Request, res: Response, next: NextFunction) => {
     try {
         const newRoom = await createRoom(req.body)
         res.json(newRoom);
     } catch (error) {
-        console.error('An error ocurred', error);
-        res.status(500).json({ error });
+        next(error);
     }
 })
 
-roomController.put("/:id", async (req: Request, res: Response, _next: NextFunction) => {
+roomController.put("/:id", async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params;
     try {
         const roomToEdit = await editRoom(id, req.body)
         res.json(roomToEdit);
     } catch (error) {
-        console.error('An error ocurred', error);
-        res.status(500).json({ error });
+        next(error);
     }
 })
 
-roomController.delete("/:id", async (req: Request, res: Response, _next: NextFunction) => {
+roomController.delete("/:id", async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params;
     try {
         const roomToDelete = await deleteRoom(id)
         res.json(roomToDelete);
     } catch (error) {
-        console.error('An error ocurred', error);
-        res.status(500).json({ error });
+        next(error);
     }
 })
