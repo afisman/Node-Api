270a557953348116a95547b24213ab8d6b2431db
afisman <afisman@gmail.com>
2024-04-13 20:58:28 +0200

    fixed user service

--- services/user.ts
+++ services/user.ts
@@ -6,57 +6,52 @@ import { compareHash, hashPassword } from '../util/bcryptUtil';
 
 
 export const fetchAllUsers = async (): Promise<UserInterface[]> => {
-    try {
-        return await User.find();
-    } catch (error) {
-        throw new AppError({ status: 500, message: "internal server error" })
+    const users = await User.find();
+    if (users === null) {
+        throw new AppError({ status: 404, message: "Users not found" });
     }
+    return users;
 }
 
 export const fetchSingleUser = async (id: any): Promise<UserInterface | null> => {
-    try {
-        return await User.findById(id);
-    } catch (error) {
-        throw new AppError({ status: 500, message: "internal server error" })
+    const user = await User.findById(id);
+    if (user === null) {
+        throw new AppError({ status: 404, message: "User not found" });
     }
+    return user;
+
 }
 
 export const createUser = async (data: UserInterface): Promise<UserInterface> => {
-    try {
-        const rawPassword = data.password;
-        const hashedPassword: string = hashPassword(rawPassword)
+    const rawPassword = data.password;
+    const hashedPassword: string = hashPassword(rawPassword)
 
-        const newUser = new User({ ...data, password: hashedPassword });
-        return await newUser.save();
-    } catch (error) {
-        throw new AppError({ status: 500, message: "internal server error" })
+    const newUser = await User.create({ ...data, password: hashedPassword })
+    if (newUser === null) {
+        throw new AppError({ status: 404, message: "User couldn't be created" });
     }
-
-
+    return newUser;
 }
 
 export const editUser = async (id: any, data: UserInterface): Promise<UserInterface | null> => {
-    try {
-        const userExists = await User.findById(id);
+    const userExists = await User.findById(id);
 
+    if (userExists === null) {
+        throw new AppError({ status: 404, message: "User not found" });
+    }
 
-        if (userExists && !compareHash(data.password, userExists.password)) {
-
-            const hashedPassword = hashPassword(data.password)
-
-            return await User.findByIdAndUpdate(id, { ...data, password: hashedPassword }, { new: true })
-        } else {
-            return await User.findByIdAndUpdate(id, data, { new: true });
-        }
-    } catch (error) {
-        throw new AppError({ status: 500, message: "internal server error" })
+    if (userExists && !compareHash(data.password, userExists.password)) {
+        const hashedPassword = hashPassword(data.password)
+        return await User.findByIdAndUpdate(id, { ...data, password: hashedPassword }, { new: true })
+    } else {
+        return await User.findByIdAndUpdate(id, data, { new: true });
     }
 }
 
 export const deleteUser = async (id: any): Promise<UserInterface | null> => {
-    try {
-        return await User.findByIdAndDelete(id);
-    } catch (error) {
-        throw new AppError({ status: 500, message: "internal server error" })
+    const user = await User.findByIdAndDelete(id);
+    if (user === null) {
+        throw new AppError({ status: 404, message: "User not found" });
     }
+    return user;
 }
