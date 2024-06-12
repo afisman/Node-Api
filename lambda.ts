c80026e6bb9d583d1f477efa286e8900f56940e7
afisman <afisman@gmail.com>
2024-06-12 15:37:59 +0200

    fixed cors issue

--- lambda.ts
+++ lambda.ts
@@ -2,5 +2,12 @@ import { app } from './app'
 import serverless from 'serverless-http'
 
 export const handler = serverless(app, {
-    response: { headers: { 'Access-Control-Allow-Origin': '*' } }
+    response: {
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type,Authorization',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,PUT,GET,DELETE'
+        }
+    }
 })
